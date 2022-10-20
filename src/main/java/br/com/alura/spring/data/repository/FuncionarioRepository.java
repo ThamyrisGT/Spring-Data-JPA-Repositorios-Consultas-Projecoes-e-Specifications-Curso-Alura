96afb4442eb789b13e57106ff3d944a9e03ecc76
thamyris <thamyris.thurler@nuvemshop.com.br>
2022-10-20 20:17:03 -0300

    Criado query dinamica com Specification

--- src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
+++ src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Repository;
 import br.com.alura.spring.data.orm.Funcionario;
 
 @Repository
-public interface FuncionarioRepository extends PagingAndSortingRepository<Funcionario, Integer> {
+public interface FuncionarioRepository extends PagingAndSortingRepository<Funcionario, Integer> , JpaSpecificationExecutor<Funcionario>  {
 	List<Funcionario> findByNome(String nome);
 	
 	@Query("SELECT f FROM Funcionario f WHERE f.nome = :nome "
